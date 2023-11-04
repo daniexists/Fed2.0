8751ffe7413b20d865e805809653c507fc16d9fe
daniexists <106262663+daniexists@users.noreply.github.com>
2023-11-04 15:43:20 +0100

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,2 +1,26 @@
 // JavaScript Document
 console.log("hi");
+
+
+
+var deCheckbox = document.querySelector("footer form label:nth-of-type(2) input");
+
+var deInput = document.querySelector("footer form  > input");
+
+var deButton = document.querySelector("footer form > button");
+
+
+deCheckbox.onchange = togglezoekveld; 
+
+function togglezoekveld(){
+
+    if (deInput.disabled == true){
+        deInput.disabled = false; 
+        deButton.disabled = false; 
+    }
+
+    else{
+        deInput.disabled = true; 
+        deButton.disabled = true; 
+    }
+}
